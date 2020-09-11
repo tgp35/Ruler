d9d709dc24900a9cc9c960c384af350cef129a7d
Thomas Pendock <70925092+tgp35@users.noreply.github.com>
2020-09-11 15:56:02 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <stdio.h>
 
+int ruler( int height ) {
+  if(height == 1) {
+    printf("-\n");
+  } else {
+    ruler(height - 1);
+    for(int i = 0; i < height; i ++) {
+      printf("-");
+    }
+    printf("\n");
+    ruler(height - 1);
+  }
+  return height;
+}
+
+int main() {
+  ruler(5);
+}
